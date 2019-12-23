0cfbfb5c389871bb7a3de184490ac3236ea69446
zmq <zmq396619664@126.com>
2019-12-23 15:45:09 +0800

    再次修改快排

--- quicksort.py
+++ quicksort.py
@@ -1,4 +1,4 @@
-#快速排序法#
+#快速排序法:最差时间复杂度O(N2),平均时间复杂度O（NlogN #
 def quick(l,left,right):
     i=left
     j=right
@@ -27,9 +27,6 @@ def quick(l,left,right):
     quick(l,0,i-1)
     quick(l,i+1,right)
     print(l)
-
-
-#最差时间复杂度O(N2),平均时间复杂度O（NlogN）   
 l=[27,24,63,56,59,0,1]
 
 #l=[13,2,56,24,8,7,9,23,63,13]
